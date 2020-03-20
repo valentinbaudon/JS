d77ee22b8e322e11e5c297b3ea9277aa4840f5cd
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-03-20 09:12:27 +0100

    com

--- string.js
+++ string.js
@@ -1,3 +1,13 @@
 function repeat(s,n){
     return s.repeat(n);
 }
+
+function truncate(s,n){
+    let a = s.substring(0,n);
+    if (a.length < n-3){
+        return a + "...";
+    }else{
+        return s;
+    }
+
+}
