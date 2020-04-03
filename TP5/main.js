3aaa97eb1890af25581e5edcf32514657b4d7319
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-04-03 09:12:46 +0200

    commit

--- TP5/main.js
+++ TP5/main.js
@@ -1,14 +1,5 @@
 function main(){
-    sumTest();
-    sumTest2();
-    lWTest();
-    rangeTest();
-    occTest();
-    flattenTest();
-    let tableau = ["Bob", "Julien", "Roger" ];
-    console.log( tableau.join(", "));
-    tableau.pop();
-    tableau.push("coco");
+    
 }
 
 main();
