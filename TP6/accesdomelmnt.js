c3e294805389e1ee0e5dadfcc13d1544333f086b
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-04-10 16:39:24 +0200

    commit

--- TP6/accesdomelmnt.js
+++ TP6/accesdomelmnt.js
@@ -5,10 +5,10 @@ function access{
     console.log(document.getElementsByClassName("titre")[0]);
     console.log(document.querySelectorAll("p").length);
     console.log(document.querySelectorAll("img").length);
-    console.log(document.querySelectorAll(".synopsis").length);
+    console.log(document.querySelectorAll(".synopsis > h2").length);
     console.log(document.querySelector("h1"));
     console.log(document.getElementById("2016").querySelectorAll(".film").length);
-    console.log(document.getElementsByClassName("titre")[1].querySelectorAll("p"));
+    console.log(document.querySelectorAll("film > .synopsis > div > p")[1]);
 
 
 }
