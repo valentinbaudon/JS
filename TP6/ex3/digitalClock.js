bf5399b82a3c6d10e085f132b72ba5b1f2c21509
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-05-06 11:08:51 +0200

    commit

---
+++ TP6/ex3/digitalClock.js
@@ -0,0 +1,43 @@
+﻿function addSegments(digitId){
+
+    // TODO : compléter
+
+}
+
+function updateDigit(digitId, value){
+
+    let segmentStates = [
+        [1, 1, 1, 0, 1, 1, 1],
+        [0, 0, 1, 0, 0, 1, 0],
+        [1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 1, 0, 1, 1],
+        [0, 1, 1, 1, 0, 1, 0],
+        [1, 1, 0, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 1, 1]
+    ];
+
+    // TODO : compléter
+}
+
+
+
+
+function init(){
+
+    // TODO : compléter
+}
+
+
+
+function main(){
+
+    init();
+
+    // TODO : compléter
+}
+
+
+main();
