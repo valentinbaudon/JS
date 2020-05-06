03b0d99eb6b9aa08f1df684593bd3083c7022db4
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-05-06 10:49:13 +0200

    commit

--- TP6/ex2/accessdata.js
+++ TP6/ex2/accessdata.js
@@ -3,8 +3,8 @@ function access(){
     console.log(document.getElementsByTagName("tr").length);
     let nbColonne = (document.getElementsByTagName("td").length+document.getElementsByTagName("th").length)/document.getElementsByTagName("tr").length;
     console.log(nbColonne);
-    console.log(querySelectorAll(".c1").length);
-    console.log(document.querySelectorAll("p").length);
+    console.log(document.querySelectorAll(".c1").length);
+    console.log(document.querySelectorAll(".c1 > td")[4]);
     console.log(document.querySelectorAll("img").length);
     console.log(document.querySelectorAll(".synopsis > h2").length);
     console.log(document.querySelector("h1").innerHTML);
