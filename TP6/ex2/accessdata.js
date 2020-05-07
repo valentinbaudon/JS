f3095ac09cb81c498c0afa4346aab0db14c4c01e
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-05-07 14:31:37 +0200

    commit

--- TP6/ex2/accessdata.js
+++ TP6/ex2/accessdata.js
@@ -1,22 +1,17 @@
 function access(){
-    console.log(document.title);
+    
+    let d = document.querySelector("#afterTable");
+    let titre = document.title;
+    let nbLigne = document.getElementsByTagName("tr").length;
+    let nbColonne = document.getElementsByTagName("th").length;
+    let nbSemestre = document.querySelectorAll(".c1").length;
+    let noteS5E1 = document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].innerText;
+    let noteS5E2 = document.querySelectorAll("tr")[10].getElementsByTagName("td")[2].innerText
 
-    console.log(document.getElementsByTagName("tr").length);
+    d.innerText = "Titre : "+titre+"\nNombre de lignes : "+nbLigne+"\nNombre de colonnes : "+nbColonne+"\nNombre de semestres : "+nbSemestre+"\nNote de S5 de E1 : "+noteS5E1+"\nNote de S5 de E2 : "+noteS5E2+"\n";
 
-    let nbColonne = (document.getElementsByTagName("td").length+document.getElementsByTagName("th").length)/document.getElementsByTagName("tr").length;
-    console.log(nbColonne);
-
-    console.log(document.querySelectorAll(".c1").length);
-
-    let note1 = document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].innerText;
-    console.log(note1);
-
-    console.log(document.querySelectorAll("tr")[10].getElementsByTagName("td")[2].innerText);
-
-    console.log(document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].innerText = parseInt(note1)+2);
-
+    document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].innerText = parseInt(noteS5E1)+2;
     document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].style.fontSize = "25px";
-
     document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].style.color = "red";
 
     let zoneTxt = document.createElement("textarea");
