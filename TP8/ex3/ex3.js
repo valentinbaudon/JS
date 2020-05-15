a45270bfb08b22464ea2abbcbadbc1987359956c
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-05-15 09:37:40 +0200

    commit

--- TP8/ex3/ex3.js
+++ TP8/ex3/ex3.js
@@ -1,9 +1,67 @@
-function survol(MouseEvent){
-    let button = document.getElementById("ex1");
-    button.style.backgroundColor = "rgb(0,125,255)";
-    console.log("x ="+MouseEvent.clientX+"   y ="+MouseEvent.clientY);
+
+let fenetre;
+
+function popup(){
+    fenetre = window.open("https://www.isen.fr/");  
+}
+function popupclose(){
+    fenetre.close();
 }
 
 
-let okButton = document.getElementById("ex1");
-okButton.addEventListener("mouseover",survol);
+let okButton = document.getElementById("id1");
+okButton.addEventListener("clic",popup);
+
+let okButton2 = document.getElementById("id2");
+okButton.addEventListener("clic",popupclose);
+
+
+
+
+
+
+
+
+
+function moveTop(){
+    let photo = document.getElementById("img");
+    photo.style.backgroundPosition = "top";
+}
+
+function moveLeft(){
+    let photo = document.getElementById("img");
+    photo.style.backgroundPosition = "left";
+}
+
+function moveRight(){
+    let photo = document.getElementById("img");
+    photo.style.backgroundPosition = "right";
+}
+
+function moveBottom(){
+    let photo = document.getElementById("img");
+    photo.style.backgroundPosition = "bottom";
+}
+
+let Button = document.getElementById("haut");
+okButton.addEventListener("clic",moveTop);
+
+let Button2 = document.getElementById("gauche");
+okButton.addEventListener("clic",moveLeft);
+
+let Button3 = document.getElementById("droite");
+okButton.addEventListener("clic",moveRight);
+
+let Button4 = document.getElementById("bas");
+okButton.addEventListener("clic",moveBottom);
+
+
+
+
+
+
+
+
+
+
+
