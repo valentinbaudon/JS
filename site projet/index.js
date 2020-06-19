9ce031b80a544f11191e080ddf8c9d473b4f2866
valentinbaudon <valentin.baudon@isen-ouest.yncrea.fr>
2020-06-19 08:59:14 +0200

    commit

--- site projet/index.js
+++ site projet/index.js
@@ -1,7 +1,7 @@
 let fenetre;
 
 function popup(){
-    fenetre = window.open("https://www.isen.fr/");  
+    fenetre = window.open("https://docs.google.com/spreadsheets/d/1-w823MbQIV07wWIZYEoNHMDf75ToaMFLGbxnDWXUB_8/edit?usp=drive_web&ouid=101663960201121362377");  
 }
 
 let okButton = document.getElementById("id1");
